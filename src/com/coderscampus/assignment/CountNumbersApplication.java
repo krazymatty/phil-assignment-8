cf8ef6d5a17d1885f8cd929e3cef75d1ad7dfd45
Phil Matthews <phil@viralwebtactics.com>
2023-01-12 13:39:10 -0700

    Reverted changes..VS Code doesn't work well with java

--- src/com/coderscampus/assignment/CountNumbersApplication.java
+++ src/com/coderscampus/assignment/CountNumbersApplication.java
@@ -13,7 +13,7 @@ public class CountNumbersApplication {
 	public static void main(String[] args) {
 		Assignment8 assignment8 = new Assignment8();
 		List<Integer> allNumbers = Collections.synchronizedList(new ArrayList<>(1000));
-		List<CompletableFuture<Void>> tasks = new ArrayList<>(1000);
+		List<CompletableFuture<Void>> tasks = Collections.synchronizedList(new ArrayList<>(1000));
 		ExecutorService executor = Executors.newCachedThreadPool();
 
 		for (int i = 0; i < 1000; i++) {
